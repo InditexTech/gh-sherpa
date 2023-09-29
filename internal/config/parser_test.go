2530f8826f6f45e8932871ca37bd762ac50a353d
Daniel Sánchez Navarro <danielsn@ext.inditex.com>
2023-09-29 11:56:25 +0200

    test: jira config template test

---
+++ internal/config/parser_test.go
@@ -0,0 +1,129 @@
+package config
+
+import (
+	"bytes"
+	"os"
+	"path/filepath"
+	"testing"
+	"text/template"
+
+	"github.com/InditexTech/gh-sherpa/internal/domain/issue_types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestWriteTemplatedConfigFile(t *testing.T) {
+
+	t.Run("Written config file should be the same as the expected default-config", func(t *testing.T) {
+		originalGetConfigFile := GetConfigFile
+		defer func() {
+			GetConfigFile = originalGetConfigFile
+		}()
+
+		GetConfigFile = func() (ConfigFile, error) {
+			tempDirName, err := os.MkdirTemp("", "sherpa-config-test")
+			return ConfigFile{
+				Path: filepath.Join(tempDirName, configPath),
+				Name: configName,
+				Type: configType,
+			}, err
+		}
+
+		err := Initialize(false)
+		require.NoError(t, err)
+
+		defaultConfigFile, err := os.ReadFile("default-config.yml")
+		require.NoError(t, err)
+
+		cfg := GetConfig()
+		var buff bytes.Buffer
+		templateData := configFileTemplateData{
+			JiraData: JiraTemplateConfiguration{
+				Jira: cfg.Jira,
+			},
+			GithubData: GithubTemplateConfiguration{
+				Github: cfg.Github,
+			},
+			BranchesData: BranchesTemplateConfiguration{
+				Branches: cfg.Branches,
+			},
+		}
+
+		err = writeTemplatedConfigFile(&buff, templateData)
+		require.NoError(t, err)
+
+		require.Equal(t, string(defaultConfigFile), buff.String())
+
+	})
+}
+
+func TestJiraTemplateConfiguration(t *testing.T) {
+	tmpl, err := template.ParseFS(embeddedTemplates, "templates/*.tmpl")
+	require.NoError(t, err)
+
+	t.Run("Should generate empty configuration", func(t *testing.T) {
+		jiraData := JiraTemplateConfiguration{
+			Jira: Jira{},
+		}
+
+		var buff bytes.Buffer
+		err := tmpl.ExecuteTemplate(&buff, "jiraConfiguration", jiraData)
+		require.NoError(t, err)
+
+		require.Equal(t, `# Jira configuration -----------------------------------#
+jira:
+  # Jira authentication configuration
+  auth:
+    # Jira authentication url to generate PAT
+    # WARNING: Replace it with your actual Jira authentication url
+    host: 
+    # Jira already generated PAT
+    # WARNING: Replace it with your actual Jira PAT
+    token: 
+    # Jira insecure TLS configuration
+    skip_tls_verify: false
+  # Jira issue types configuration
+  issue_types: {}
+`, buff.String())
+	})
+
+	t.Run("Should generate configuration with values", func(t *testing.T) {
+		jiraData := JiraTemplateConfiguration{
+			Jira: Jira{
+				Auth: JiraAuth{
+					Host:        "https://jira.example.com",
+					Token:       "jira-pat",
+					InsecureTLS: true,
+				},
+				IssueTypes: map[issue_types.IssueType][]string{
+					issue_types.Bug:         {"1"},
+					issue_types.Feature:     {"2", "3", "4"},
+					issue_types.Improvement: {},
+				},
+			},
+		}
+
+		var buff bytes.Buffer
+		err := tmpl.ExecuteTemplate(&buff, "jiraConfiguration", jiraData)
+		require.NoError(t, err)
+
+		require.Equal(t, `# Jira configuration -----------------------------------#
+jira:
+  # Jira authentication configuration
+  auth:
+    # Jira authentication url to generate PAT
+    # WARNING: Replace it with your actual Jira authentication url
+    host: https://jira.example.com
+    # Jira already generated PAT
+    # WARNING: Replace it with your actual Jira PAT
+    token: jira-pat
+    # Jira insecure TLS configuration
+    skip_tls_verify: true
+  # Jira issue types configuration
+  issue_types: 
+    bug: ["1"]
+    feature: ["2", "3", "4"]
+    improvement: []
+`, buff.String())
+
+	})
+}
